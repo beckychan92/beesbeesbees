08251956eff1be2334cef4fa73db35fbffee06fd
beckychan92 <beckychan92@gmail.com>
2018-01-22 11:41:28 -0800

    bees completed

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.color = 'yellow';
+    this.honeyPot = 0;
+    this.job = 'make honey';
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee
+
+
+HoneyMakerBee.prototype.makeHoney = function(){
+    this.honeyPot = this.honeyPot + 1;
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+    if(this.honeyPot > 1){
+        this.honeyPot--;
+    }
+}
